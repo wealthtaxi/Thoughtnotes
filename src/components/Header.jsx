4f108470feb46435d7a68584e38532f030c6ff98
Moore Greenberg <64972059+wealthtaxi@users.noreply.github.com>
2021-02-15 11:03:38 -0800

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import WbSunnyIcon from "@material-ui/icons/WbSunny";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <WbSunnyIcon />
+        Thoughts
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
